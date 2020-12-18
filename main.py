c84792b2a7880a4c35268928c52cf98a6e4dc38e
Mike Castro <mkcastro24@gmail.com>
2020-12-18 18:11:45 +0800

    change output from csv to a single json

--- main.py
+++ main.py
@@ -1,32 +1,28 @@
 # %%
-import csv
+import json
 
 import requests
 
 
 # %%
 def main():
+    container = []
     for page in range(1, 16):
-        data = get_data(page)
-        if page == 1:
-            keys = data[0].keys()
+        container.extend(get_data(page))
 
-        write(keys, data, str(page))
+    write(container)
 
 
 def get_data(page):
     return requests.get(
-        "http://api.linode.com/v4/linode/stackscripts?page=" + str(page)
+        "http://api.linode.com/v4/linode/stackscripts?page={}".format(page)
     ).json()["data"]
 
 
-def write(keys, rows, filename):
-    with open("stackscripts/{}.csv".format(filename), "w") as output_file:
-        writer = csv.DictWriter(output_file, keys)
-        writer.writeheader()
-        writer.writerows(rows)
+def write(data):
+    with open("output.json", "w") as outfile:
+        json.dump(data, outfile)
 
 
-# %%
 if __name__ == "__main__":
     main()
