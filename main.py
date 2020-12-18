971671ac342a876991ce74b56bddb8c2a660d317
Mike Castro <mkcastro24@gmail.com>
2020-12-18 17:26:49 +0800

    implement

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-#%%
-# make requests
-# turn the response into something workable
-# convert it into a csv
-# loop
-# search
+# %%
+import csv
+
+import requests
+
+
+# %%
+def main():
+    for page in range(1, 16):
+        data = get_data(page)
+        if page == 1:
+            keys = data[0].keys()
+
+        write(keys, data, str(page))
+
+
+def get_data(page):
+    return requests.get(
+        "http://api.linode.com/v4/linode/stackscripts?page=" + str(page)
+    ).json()["data"]
+
+
+def write(keys, rows, filename):
+    with open("stackscripts/{}.csv".format(filename), "w") as output_file:
+        writer = csv.DictWriter(output_file, keys)
+        writer.writeheader()
+        writer.writerows(rows)
+
+
+# %%
+if __name__ == "__main__":
+    main()
